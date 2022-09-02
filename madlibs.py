f05b55dc22c6d508377e70cada54e3c49f94763e
unknown <maier.samuel99@gmail.com>
2022-09-02 14:35:20 +0300

    Some information about string concatenation.

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+#string concatenation or how to put strings togheter
+#we want to create a string that says "subscribe to ______"
+#youtuber = "Samuel" #some string variable
+
+#a few ways to do this:
+#print("subscribe to " + youtuber) #not so good
+#print("subscribe to {}".format(youtuber)) #ok
+#print(f"subscribe to {youtuber}") #the most staightforward
