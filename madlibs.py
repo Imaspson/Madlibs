d0e045e78eb900ba9ac33db8da766c794fd35c44
unknown <maier.samuel99@gmail.com>
2022-09-02 14:57:19 +0300

    Added the madlib variable and initialized all the variables in it.

--- madlibs.py
+++ madlibs.py
@@ -6,3 +6,12 @@
 #print("subscribe to " + youtuber) #not so good
 #print("subscribe to {}".format(youtuber)) #ok
 #print(f"subscribe to {youtuber}") #the most staightforward
+
+adj = str(input("Adjective: "))
+verb1 = str(input("First verb: "))
+verb2 = str(input("Second verb: "))
+famous_person = str(input("Famous person: "))
+
+madlib = f"Computer programming is so {adj}! It makes me so excited \
+all the time because I love to {verb1}. Stay hydrated and {verb2} \
+like you are {famous_person}!"
